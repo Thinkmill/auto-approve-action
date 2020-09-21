bbd4f8d701b5b5a49d93ff3e09e2af1914c199aa
mitchellhamilton <mitchell@hamil.town>
2020-09-21 14:10:09 +1000

    Fix build

--- dist/index.js
+++ dist/index.js
@@ -5760,6 +5760,17 @@ function wrappy (fn, cb) {
 
 "use strict";
 
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
@@ -5779,63 +5790,118 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+var _a;
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core = __importStar(__webpack_require__(186));
-const github = __importStar(__webpack_require__(438));
-let githubToken = process.env.GITHUB_TOKEN;
+var core = __importStar(__webpack_require__(186));
+var github = __importStar(__webpack_require__(438));
+var githubToken = process.env.GITHUB_TOKEN;
 if (!githubToken) {
     core.setFailed("Please add the GITHUB_TOKEN to this action");
     process.exit(1);
 }
-const pull_number = github.context.payload.pull_request?.number;
+var pull_number = (_a = github.context.payload.pull_request) === null || _a === void 0 ? void 0 : _a.number;
 if (pull_number === undefined) {
     core.setFailed("This action must only be run on pull_request events");
     process.exit(1);
 }
-const octokit = github.getOctokit(githubToken);
-const approvedString = core.getInput("approved", { required: true });
-const approved = approvedString === "true"
+var octokit = github.getOctokit(githubToken);
+var approvedString = core.getInput("approve", { required: true });
+var approved = approvedString === "true"
     ? true
     : approvedString === "false"
         ? false
-        : (() => {
-            core.setFailed(`The "approved" argument must be either "true" or "false" but was ${JSON.stringify(approvedString)}`);
+        : (function () {
+            core.setFailed("The \"approve\" argument must be either \"true\" or \"false\" but was " + JSON.stringify(approvedString));
             process.exit(1);
         })();
-async function getLastReviewFromActionsBot() {
-    const reviews = await octokit.pulls.listReviews({
-        ...github.context.repo,
-        pull_number,
-        per_page: 100,
-    });
-    for (const review of [...reviews.data].reverse()) {
-        if (review.user.login === "github-actions[bot]") {
-            return review;
-        }
-    }
-}
-(async () => {
-    const pull_number = 1;
-    const lastReviewFromActionsBot = await getLastReviewFromActionsBot();
-    if (approved) {
-        if (lastReviewFromActionsBot?.state === "DISMISSED" ||
-            lastReviewFromActionsBot === undefined) {
-            await octokit.pulls.createReview({
-                ...github.context.repo,
-                event: "APPROVE",
-                pull_number,
-            });
-        }
-    }
-    else if (lastReviewFromActionsBot?.state === "APPROVED") {
-        octokit.pulls.dismissReview({
-            ...github.context.repo,
-            pull_number,
-            message: "The condition that made this PR approved changed to false, a person must approve this now.",
-            review_id: 2,
+function getLastReviewFromActionsBot() {
+    return __awaiter(this, void 0, void 0, function () {
+        var reviews, _i, _a, review;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0: return [4 /*yield*/, octokit.pulls.listReviews(__assign(__assign({}, github.context.repo), { pull_number: pull_number, per_page: 100 }))];
+                case 1:
+                    reviews = _b.sent();
+                    for (_i = 0, _a = __spreadArrays(reviews.data).reverse(); _i < _a.length; _i++) {
+                        review = _a[_i];
+                        if (review.user.login === "github-actions[bot]") {
+                            return [2 /*return*/, review];
+                        }
+                    }
+                    return [2 /*return*/];
+            }
         });
-    }
-})().catch((err) => {
+    });
+}
+(function () { return __awaiter(void 0, void 0, void 0, function () {
+    var pull_number, lastReviewFromActionsBot;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                pull_number = 1;
+                return [4 /*yield*/, getLastReviewFromActionsBot()];
+            case 1:
+                lastReviewFromActionsBot = _a.sent();
+                if (!approved) return [3 /*break*/, 4];
+                if (!((lastReviewFromActionsBot === null || lastReviewFromActionsBot === void 0 ? void 0 : lastReviewFromActionsBot.state) === "DISMISSED" ||
+                    lastReviewFromActionsBot === undefined)) return [3 /*break*/, 3];
+                return [4 /*yield*/, octokit.pulls.createReview(__assign(__assign({}, github.context.repo), { event: "APPROVE", pull_number: pull_number }))];
+            case 2:
+                _a.sent();
+                _a.label = 3;
+            case 3: return [3 /*break*/, 5];
+            case 4:
+                if ((lastReviewFromActionsBot === null || lastReviewFromActionsBot === void 0 ? void 0 : lastReviewFromActionsBot.state) === "APPROVED") {
+                    octokit.pulls.dismissReview(__assign(__assign({}, github.context.repo), { pull_number: pull_number, message: "The condition that made this PR approved changed to false, a person must approve this now.", review_id: 2 }));
+                }
+                _a.label = 5;
+            case 5: return [2 /*return*/];
+        }
+    });
+}); })().catch(function (err) {
     console.error(err);
     core.setFailed(err.message);
 });
